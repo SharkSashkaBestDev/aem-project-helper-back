8df1a3fd18657268304c1b6ab8cfe4c803f0447e
SharkSashkaBestDev <myagkiyalexandr@gmail.com>
2018-06-17 11:39:01 +0300

    initial

---
+++ src/main/java/com/csg/asoft/aemprojecthelper/AemProjectHelperApplication.java
@@ -0,0 +1,12 @@
+package com.csg.asoft.aemprojecthelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AemProjectHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AemProjectHelperApplication.class, args);
+	}
+}
